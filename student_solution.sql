eb9e6acc978b4d8c9d57f9f494e06c1f2dae6ac1
SARAVANAN K <sarodeivl215@gmail.com>
2026-09-03 10:02:39 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,1 +1,27 @@
+CREATE TABLE Department78(
+DepartmentID INT,
+DepartmentName VARCHAR(30)
+);
 
+INSERT INTO Department78 VALUES
+(101,'Computer Science'),
+(102,'Mathematics'),
+(103,'Physics');
+
+CREATE TABLE Student78(
+StudentID INT,
+StudentName VARCHAR(20),
+DepartmentID INT
+);
+
+INSERT INTO Student78 VALUES
+(1001,'Arun',101),
+(1002,'Divya',102),
+(1003,'Karthik',101),
+(1004,'Nisha',103);
+
+SELECT Student78.StudentName,
+Department78.DepartmentName
+FROM Student78
+INNER JOIN Department78
+ON Student78.DepartmentID = Department78.DepartmentID;
